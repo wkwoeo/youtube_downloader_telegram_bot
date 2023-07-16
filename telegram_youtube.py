f15cbe04d22686be992fe62c2b23c56d9e7117a7
Mohammed alhamdi <104478279+wkwoeo@users.noreply.github.com>
2023-07-16 20:14:57 +0300

    Update telegram_youtube.py

--- telegram_youtube.py
+++ telegram_youtube.py
@@ -7,7 +7,7 @@ import threading
 
 # find port of server 
 PORT = int(os.environ.get('PORT',5000))
-token = '5728368003:AAFGFs7r1LAJB-flrJ9et5J7Okzgw1_4s_Y'
+token = '6232454530:AAGwVyhkV1sdBKwmAZWKo82iqMEDOlHfixw'
 
 START_CO, GET_WORD, GET_NUMBER,GET_CHANNEL_URL, GET_URL, CONFIRMATION = range(1, 7)
 
